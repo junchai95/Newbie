9c033c9a6ef4049abdba325749ed0863e4440d55
junchai95 <71997938+junchai95@users.noreply.github.com>
2020-10-01 09:45:52 +0800

    Update try.sh

--- try.sh
+++ try.sh
@@ -1,3 +1,7 @@
-#!/bin/sh
-echo "Hello, world!";
-printf 'Testing'
+#!/bin/bash
+# Passing arguments to a function
+print_something () {
+echo Hello $1
+}
+print_something Adrian
+print_something Kelvin
